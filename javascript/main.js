c80fd5faec0a01cbe3f2fc54fe189dbef7eaf2b4
Mariana-1324 <cindarojas2007@cesanrafael.org>
2024-11-19 14:30:14 -0400

    ProyectosWebMariana

---
+++ javascript/main.js
@@ -0,0 +1,41 @@
+let mensaje="HolaMundo";
+console.log(mensaje)
+let numeroUno=2;
+let numeroDos=4;
+let numeroTres=10;
+let resultadoSuma=numeroUno+numeroDos+numeroTres;
+let resultadoResta=numeroUno-numeroDos-numeroTres;
+let resultadoDivicion=numeroUno/numeroDos;
+let resultadoMultiplicacion=numeroUno*numeroDos*numeroTres;
+console.log("la suma es ",resultadoSuma);
+console.log("la resta es ",resultadoResta);
+console.log("la divicion es ",resultadoDivicion);
+console.log("la multiplicacion es ",resultadoMultiplicacion);
+function sumar(){
+    let numeroUno=3;
+    let numeroDos=4;
+    let numeroTres=10;
+    let resultado =numeroUno+numeroDos+numeroTres;
+    alert ("la sumar es "+resultado);
+}
+function resta(){
+    let numeroUno=3;
+    let numeroDos=4;        
+    let numeroTres=10;
+    let resultado =numeroUno-numeroDos-numeroTres;
+    alert ("la resta es "+resultado);
+}
+function dividir(){
+    let numeroUno=3;
+    let numeroDos=4;
+    let numeroTres=10;
+    let resultado =numeroUno/numeroDos/numeroTres;
+    alert ("la dividir es "+resultado);
+}
+function multiplicacion(){
+    let numeroUno=3;
+    let numeroDos=4;
+    let numeroTres=10;
+    let resultado =numeroUno*numeroDos*numeroTres;
+    alert ("la multiplicacion es "+resultado);
+}
